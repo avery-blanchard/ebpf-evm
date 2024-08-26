7547e5a676a89332ac80330d8569603a2dc8fe40
Avery Blanchard <ab1036@duke.edu>
2024-08-26 09:21:55 -0400

    Outline kernel module functions

---
+++ ebpf_helpers.c
@@ -0,0 +1,117 @@
+#define _GNU_SOURCE
+#define MODULE_NAME "eBPF-EVM"
+
+#include "ebpf_helpers.h"
+#include <linux/unistd.h>
+#include <linux/mount.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
+#include <linux/printk.h>
+include <linux/evm.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/kprobes.h>
+#include <linux/integrity.h>
+#include <uapi/linux/bpf.h>
+#include <linux/bpf.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/cred.h>
+#include <linux/fcntl.h>
+#include <linux/bpf_trace.h>
+#include <uapi/linux/bpf.h>
+#include <linux/bpf_lirc.h>
+#include <linux/security.h>
+#include <linux/lsm_hooks.h>
+#include <linux/btf.h>
+#include <linux/btf_ids.h>
+#include <linux/sysfs.h>
+#include <linux/bpfptr.h>
+#include <linux/btf_ids.h>
+
+BTF_SET8_START(evm_kfunc_ids)
+BTF_ID_FLAGS(func, bpf_process_measurement, KF_TRUSTED_ARGS | KF_SLEEPABLE)
+BTF_ID_FLAGS(func,  ima_file_measure, KF_TRUSTED_ARGS | KF_SLEEPABLE)
+BTF_ID_FLAGS(func,  ima_store_measurement, KF_TRUSTED_ARGS | KF_SLEEPABLE)
+BTF_SET8_END(evm_kfunc_ids)
+
+static const struct btf_kfunc_id_set bpf_emv_kfunc_set = {
+	.owner = THIS_MODULE,
+	.set   = &evm_kfunc_ids,
+};
+static int ebpf_helpers_init(void)
+{
+	int ret;
+	
+	/* Register kernel module functions wiht libbpf */
+	ret = register_btf_kfunc_id_set(BPF_PROG_TYPE_LSM, &bpf_evm_kfunc_set);
+	if (ret < 0)
+		return ret;
+
+	/* Attach kprobe to kaalsysms_lookup_name to 
+	 * get function address (symbol no longer exported */
+	typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+	kallsyms_lookup_name_t kallsyms_lookup_name;
+	register_kprobe(&kp);
+	kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+	unregister_kprobe(&kp);
+	
+
+	
+	/* Use kallsyms_lookup_name to retrieve kernel EVM functions */
+	__vfs_getxattr = (ssize_t (*)(struct dentry *, struct inode *, const char *, void *, size_t))
+			kallsyms_lookup_name("__vfs_getxattr");
+	if (__vfs_getxattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	}	
+	vfs_getxattr = (ssize_t (*)(struct mnt_idmap *, struct dentry *, const char *,
+			void *, size_t)) kallsyms_lookup_name("vfs_getxattr");
+	if (vfs_getxattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	}
+
+	evm_verifyxattr = (enum integrity_status (*)(struct dentry *, const char *, void *, size_t))
+			kallsyms_lookup_name("evm_verifyxattr");
+	if (evm_verifyxattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	}
+
+	vfs_setxattr = (int (*)(struct mnt_idmap *, struct dentry *, const char *, 
+			const void *, size_t, int)) kallsyms_lookup_name("vfs_setxattr");
+	if (vfs_setxattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	} 
+	
+	vfs_listxattr = (int (*)(struct dentry *, char *, size_t)) kallsyms_lookup_name("vfs_listxattr");
+	if (vfs_listxattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	}
+	
+	vfs_removexattr = (int (*)(struct mnt_idmap *, struct dentry *, const char *))
+				kallsyms_lookup_name("vfs_removexattr");
+	if (vfs_removexattr == 0) {
+		pr_err("Lookup fails\n");
+		return -1;
+	}
+	return ret;	
+
+}
+static void ebpf_helpers_exit(void)
+{
+	pr_info("Exiting eBPF helpers\n");
+	return;
+}
+
+module_init(ebpf_helpers_init);
+module_exit(ebpf_helpers_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION(MODULE_NAME);
+MODULE_AUTHOR("Avery Blanchard");
---
+++ ebpf_helpers.h
@@ -0,0 +1,58 @@
+#ifndef __EBPF_EVM_H__
+#define __EBPF_EVM_H__
+
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/security.h>
+#include <linux/module.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+
+extern int register_btf_kfunc_id_set(enum bpf_prog_type prog_type,
+                              const struct btf_kfunc_id_set *kset);
+static struct kprobe kp = {
+    .symbol_name = "kallsyms_lookup_name"
+};
+
+int (*vfs_removexattr)(struct mnt_idmap *, 
+			struct dentry *,
+			const char *);
+
+int (*__vfs_removexattr)(struct mnt_idmap *,
+			 struct dentry *,
+			 const char *);
+ssize_t (*vfs_listxattr)(struct dentry *,
+			 char *, size_t);
+ssize_t (*vfs_getxattr)(struct mnt_idmap *,
+			struct dentry *,
+			const char *,
+			void *, size_t);
+ssize_t (*__vfs_getxattr)(struct dentry *, 
+			  struct inode *,
+			  const char *,
+			  void *, size_t);
+int (*__vfs_setxattr_locked)(struct mnt_idmap *,
+			   struct dentry *, 
+			const char *,
+			const void *,
+			size_t, int,
+			struct inode **);
+int (*vfs_setxattr)(struct mnt_idmap *, 
+		struct dentry *,
+		const char *,
+		const void *,
+		size_t, int);
+int (*__vfs_setxattr)(struct mnt_idmap *,
+		struct dentry *,
+		struct inode *,
+		const char *,
+		const void *,
+		size_t, int);
+int (*__vfs_removexattr_locked)(struct mnt_idmap *,
+		struct dentry *,
+		const char *,
+		struct inode **);
+
+enum integrity_status (*evm_verifyxattr)(struct dentry *,
+			const char *, void *, size_t);		  
